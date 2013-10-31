cd0d05520a777453cab09091bd71f18b2a88cee3
coolkev <kevin@lewistech.com>
2013-10-30 22:42:12 -0700

    flickering

--- public/js/app.ts
+++ public/js/app.ts
@@ -16,7 +16,7 @@ module pumpkinlights {
             $(e).bind(touchstartevent, function (evt:JQueryMouseEventObject) {
                 if (evt.clientX >= 200) {
                     socket.emit('flickerStart', {
-                        number: i, brightness: parseInt($('#brightness').val())
+                        number: i
                     });
                 }
                 else {
@@ -34,10 +34,12 @@ module pumpkinlights {
         });
 
         $('#playmusic').click(function () {
-            socket.emit('playMusic');
+            socket.emit('playMusic', {
+                file: $('#musicFiles').val()
+            });
 
         });
-
+        
         $('#stopmusic').click(function () {
             socket.emit('stopMusic');
 
@@ -46,36 +48,140 @@ module pumpkinlights {
 
         });
 
+        $('#saveRecording').click(function () {
+
+            var name = prompt('Enter filename', $('#savedRecordings').val())
+
+            if (name) {
+                socket.emit('saveRecording', { file: name });
+                $('#savedRecordings').append($('<option/>').text(name)).val(name);
+
+                
+            }
+            // $.get('/music/stop');
+
+
+        });
+
+        $('#startPlayback').click(function () {
+            socket.emit('startPlayback', { file: $('#savedRecordings').val() } );
+
+        });
+        $('#stopPlayback').click(function () {
+            socket.emit('stopPlayback');
+
+        });
+
+
+        socket.emit('getSavedRecordings', (files: string[]) => {
+
+            $('#savedRecordings').append($.map(files, m=> $('<option/>').text(m)));
+
+        });
+        socket.emit('getMusicFiles', (files: string[]) => {
+
+            $('#musicFiles').append($.map(files, m=> $('<option/>').text(m)));
+
+        });
+        
         var keyDown: boolean[] = [];
         $(document).keydown(function (evt) {
             //96 = 0
 
-            if (evt.which >= 96 && evt.which <= 104) {
-                var i = evt.which - 96;
+            //console.log('keydown: ', evt.which);
 
-                if (!keyDown[i]) {
-                    socket.emit('togglestart', { number: i });
-                    keyDown[i] = true;
-                }
+            //var i = null;
+            //if (evt.which >= 96 && evt.which <= 104) {
+            //    i = evt.which - 96;
+
+            //} else if (evt.which >= 48 && evt.which <= 56) {
+            //    i = evt.which - 48;
+
+            //}
+
+
+            //if (i !== null && !keyDown[i]) {
+            //    socket.emit('lightOn', { number: i });
+            //    keyDown[i] = true;
+            //}
+
+            var i = translateKey(evt.which);
+            
+            if (i !== null && !keyDown[i]) {
+                socket.emit('lightOn', { number: i });
+                keyDown[i] = true;
             }
+
 
         });
+
+
+         
          $(document).keyup(function (evt) {
             //96 = 0
+             // 48 = 0
 
-            if (evt.which >= 96 && evt.which <= 104) {
-                var i = evt.which - 96;
+            // var i = null;
 
-                if (keyDown[i]) {
-                    socket.emit('toggleend', { number: i });
-                    keyDown[i] = false;
-                }
-            }
+            //if (evt.which >= 96 && evt.which <= 104) {
+            //    i = evt.which - 96;
+
+            //}
+            //else if (evt.which >= 48 && evt.which <= 56) {
+            //    i = evt.which - 48;
+
+            //}
+             
+            // if (i!==null && keyDown[i]) {
+            //     socket.emit('lightOff', { number: i });
+            //     keyDown[i] = false;
+            // }
+
+
+             var i = translateKey(evt.which);
+
+             if (i !== null && keyDown[i]) {
+                 socket.emit('lightOff', { number: i });
+                 keyDown[i] = false;
+             }
 
         });
 
 
     });
 
+
+    function translateKey(char: number) {
+
+        switch (char) {
+
+            case 65:
+            case 74:
+                return 1;
+
+            case 83:
+            case 75:
+                return 2;
+
+            case 68:
+            case 76:
+                return 3;
+
+            case 70:
+            case 186:
+                return 4;
+
+            case 71:
+            case 72:
+                return 5;
+            
+            case 32:
+                return 0;
+
+        }
+
+        return null;
+
+    }
 
 }
--- server.ts
+++ server.ts
@@ -1,108 +1,270 @@
 import http = require('http');
 import fs = require('fs');
 import express = require('express');
-//import routes = require('./routes');
-//import user = require('./routes/user');
 import path = require('path');
 import wiringpi = require('wiringpi');
 import shiftregister = require('ShiftRegister');
-
-var apppath = '/home/pi/app/';
-
-var spawn = require('child_process').spawn;
-var musicChild;
-
-
-var shiftRegister = new shiftregister.ShiftRegister({ latchPin: 7, clockPin: 11, dataPin: 12, registerCount: 16 });
-
-
-var app = express();
-app.set('port', process.env.PORT || 80);
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-//app.get('/music/:action', function (req, res) {
-    
-//    var start = req.params.action == 'start';
-
-//    if (start) {
-//        console.log('starting music');
-
-//        if (musicChild)
-//            musicChild.kill();
-
-//        musicChild = spawn('mpg321', [apppath + 'The Nightmare Before Christmas - This is Halloween.mp3']);
-//    }
-//    else if (musicChild) {
-//        console.log('stopping music');
-
-//        musicChild.kill();
-//        musicChild = null;
-//    }
-
-//    res.json({ success: true });
-
-
-//});
-
-var server = http.createServer(app);
-server.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
 import io = require("socket.io");
 
-io.listen(server).sockets.on('connection', function (socket) {
+module pumpkinlights {
+    
+    var apppath = '/home/pi/app/';
 
-    socket.on('flickerStart', function (data) {
+    var spawn = require('child_process').spawn;
+    var musicChild;
 
-        var lightNum = data.number;
 
-        if (lightNum == 0)
-            shiftRegister.all(m=> m.on());
-        else
-            shiftRegister.pin(lightNum - 1).flickerStart(data.brightness);
+    var shiftRegister = new shiftregister.ShiftRegister({ latchPin: 7, clockPin: 11, dataPin: 12, registerCount: 16 });
+
+
+    var app = express();
+    app.set('port', process.env.PORT || 80);
+    app.use(express.static(path.join(__dirname, 'public')));
+
+
+    var server = http.createServer(app);
+    server.listen(app.get('port'), function () {
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+
+    var currentMusicFile: string;
+
+    var recording: RecordedEvent[];
+    //var startTime: number;
+    var lastTime: number;
+
+    function recordEvent(action: string, num: number) {
+
+        if (recording) {
+            var time = new Date().getTime();
+
+            recording.push({ action: action, num: num, ms: time - lastTime });
+            //lastTime = time;
+        }
+    }
+    io.listen(server).sockets.on('connection', function (socket) {
+
+        var events = ['flickerStart', 'lightOn', 'lightOff'];
+
+        events.forEach(m=> {
+
+            socket.on(m, function (data) {
+
+
+                var lightNum = data.number;
+                recordEvent(m, lightNum);
+
+                pumpkinlights[m](lightNum);
+
+
+            });
+
+        });
+
+        socket.on('playMusic', function (data) {
+            console.log('starting music');
+
+            currentMusicFile = data.file;
+
+            playMusic(data.file);
+
+            recording = [];
+            lastTime = new Date().getTime();
+
+        });
+
+        socket.on('stopMusic', function (data) {
+            console.log('stopping music');
+
+            stopMusic();
+
+        });
+
+
+
+        socket.on('startPlayback', function (data) {
+            console.log('startPlayback');
+
+            if (data.file) {
+                startPlayback(data.file);
+            }
+            else {
+                playMusic(currentMusicFile);
+
+                replayRecording(0);
+            }
+        });
+
+
+
+        socket.on('stopPlayback', function (data) {
+            console.log('stopPlayback');
+
+            if (playbackTimer) {
+                clearTimeout(playbackTimer);
+            }
+            
+            stopMusic();
+
+        });
+
+        socket.on('getSavedRecordings', function (fn:(s:string[])=>void) {
+            console.log('getSavedRecordings');
+
+            fs.readdir(apppath + 'recordings', (err, files) => {
+                fn(files);
+            });
+        });
+
+        socket.on('getMusicFiles', function (fn: (s: string[]) => void) {
+            console.log('getMusicFiles');
+
+            fs.readdir(apppath + 'music', (err, files) => {
+                fn(files);
+            });
+        });
+
+        socket.on('saveRecording', function (data) {
+            console.log('saveRecording', data);
+
+            var jsdata = {
+                musicFile: currentMusicFile, events: recording
+            };
+
+            var json = JSON.stringify(jsdata);
+            //console.log('data to save: ', json);
+
+            fs.writeFile(apppath + 'recordings/' + data.file, json, (err) => {
+                console.log('save complete');
+
+                if (err)
+                    console.log('error: ', err);
+            });
+
+        });
 
     });
 
+    //startPlayback('full song');
 
-    socket.on('lightOn', function (data) {
 
-        var lightNum = data.number;
+    export function lightOn(lightNum) {
 
         if (lightNum == 0)
             shiftRegister.all(m=> m.on());
         else
             shiftRegister.pin(lightNum - 1).on();
 
-    });
+    }
 
-    socket.on('lightOff', function (data) {
-        var lightNum = data.number;
+
+    export function lightOff(lightNum) {
 
         if (lightNum == 0)
             shiftRegister.all(m=> m.off());
         else
             shiftRegister.pin(lightNum - 1).off();
 
-    });
+    }
 
-    socket.on('playMusic', function (data) {
-        console.log('starting music');
+    export function flickerStart(lightNum) {
+
+        if (lightNum == 0)
+            shiftRegister.all(m=> m.on());
+        else
+            shiftRegister.pin(lightNum - 1).flickerStart();
+
+
+    }
+
+    function playMusic(file: string) {
 
         if (musicChild)
             musicChild.kill();
 
-        musicChild = spawn('mpg321', [apppath + 'The Nightmare Before Christmas - This is Halloween.mp3']);
 
+        musicChild = spawn('mpg321', [apppath + 'music/' + file]);
+        
+    }
 
-    });
+    function stopMusic() {
 
-    socket.on('stopMusic', function (data) {
-        console.log('stopping music');
+        if (musicChild)
+            musicChild.kill();
 
-        musicChild.kill();
         musicChild = null;
+    }
 
-    });
-});
+
+
+
+    function startPlayback(filename) {
+
+
+        fs.readFile(apppath + 'recordings/' + filename, {}, (err, jsondata) => {
+
+            var recordedFile = <Recording>JSON.parse(jsondata);
+            currentMusicFile = recordedFile.musicFile;
+
+            recording = recordedFile.events;
+            playMusic(currentMusicFile);
+
+            replayRecording(0);
+        });
+
+
+
+    }
+
+    var playbackTimer: number;
+
+    //var recordingCounter;
+    var playbackStartTime;
+
+    export function replayRecording(eventCtr: number) {
+        console.log('replayRecording ' + eventCtr);
+
+        var time = new Date().getTime();
+
+        if (eventCtr == 0)
+            playbackStartTime = time;
+
+
+        if (eventCtr == recording.length)
+            return;
+
+
+        var event = recording[eventCtr];
+
+
+
+        var wait = Math.max(0, playbackStartTime - time + event.ms);
+
+        console.log('next action: ' + event.action + ' light: ' + event.num + ' starting in ' + wait);
+
+        playbackTimer = setTimeout(() => {
+            replayRecording(eventCtr + 1);
+
+            console.log('replaying action: ' + event.action);
+            pumpkinlights[event.action](event.num);
+        }, wait);
+
+    }
+
+    interface RecordedEvent {
+        action: string;
+        num: number;
+        ms: number;
+
+    }
+
+
+
+    interface Recording {
+
+        musicFile: string;
+        events: RecordedEvent[];
+
+    }
+
+}
